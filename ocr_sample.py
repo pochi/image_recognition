fa04c6eaf72f2d85d3944616911cd1247fad49d5
pochi <pochi.black@gmail.com>
2017-06-15 07:23:09 +0900

    Add padding on data preparation

--- ocr_sample.py
+++ ocr_sample.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 import os
+import pyocr
+import pyocr.builders
+from PIL import Image
+from IPython import embed
+from IPython.terminal.embed import InteractiveShellEmbed
 
 DATA_DIR = "./data"
 
@@ -9,21 +14,40 @@ def prepare_dir(file):
     dir = DATA_DIR + "/" + basename
     if not os.path.exists(dir):
         os.makedirs(dir)
-
     return dir
 
-def resize_to(file, dir, range=xrange(5)):
+def resize_to_xx_times(file, dir, range=xrange(5)):
     for time in range:
         im = cv2.imread(DATA_DIR + "/original/" + file)
         gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
-        _, threshold_im = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)        
-        resized_im = cv2.resize(gray, None, fx = time + 1, fy = time + 1)            
+        _, threshold_im = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        height, width = threshold_im.shape[:2]
+        # add padding : tesseract needs enough spaceas for recognition.
+        padding_im = cv2.resize(np.zeros((1, 1), np.uint8), (width+20, height+20))
+        # backgroaund change white
+        padding_im[:] = 255
+        padding_im[10:height+10, 10:width+10] = threshold_im
+        resized_im = cv2.resize(padding_im, None, fx = time + 1, fy = time + 1)            
         cv2.imwrite(dir + "/" + str(time + 1) + ".jpg", resized_im)
 
-    
+def extract_ocr(dir):
+    for file in os.listdir(dir):
+        yield pyocr_tool().image_to_string(
+            Image.open(dir + "/" + file),
+            lang="jpn+eng",
+            builder=pyocr.builders.TextBuilder()
+        )
+
+def pyocr_tool():
+    tools = pyocr.get_available_tools()
+    if len(tools) == 0:
+        print("No OCR found")
+        exit
+    return tools[0]
+
 if __name__ == '__main__':
     for file in os.listdir(DATA_DIR + "/original"):
         current_dir = prepare_dir(file)
-        resize_to(file, current_dir)
-        print current_dir
-
+        resize_to_xx_times(file, current_dir)
+        for text in extract_ocr(current_dir):
+            print text
